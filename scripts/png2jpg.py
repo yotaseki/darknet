da49eeafd25608758de145afc8e7ea19bdcb0fef
yotaseki <uyu9480s@outlook.jp>
2017-06-20 03:04:19 +0900

    update src

--- scripts/png2jpg.py
+++ scripts/png2jpg.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python 
+# DETAIL : 
+# * $1 directory has image(.png)
 import sys, os
 import cv2
 
 argv = sys.argv
-
-print "convert :" + argv[1]
-fn = argv[1]
-img = cv2.imread(fn)
-fn = fn.rstrip(".png") + ".jpg"
-print "generate :" + fn
-cv2.imwrite(fn ,img)
+line = os.listdir(argv[1])
+for i in line:
+    #print os.path.splitext(i)
+    if os.path.splitext(i)[1] == '.png':
+        fn = argv[1] + "/" + i
+        img = cv2.imread(fn)
+        fn = fn.rstrip(".png") + ".jpg"
+        print "generate :" + fn
+        cv2.imwrite(fn ,img)
